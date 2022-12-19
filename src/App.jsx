b1554137696e44f51436a5de765f7d6553da160d
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2022-12-19 16:56:02 +0100

    Create Pagination.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
 import Intro from './components/Intro';
 import Links from './components/Links';
 import Logo from './components/Logo';
+import Pagination from './components/Pagination';
 
 function App() {
+  const [page, setPage] = useState(0);
+  useEffect(() => {
+    const firstPage = document.getElementById('intro');
+    const container = document.getElementById('container');
+    container.addEventListener(
+      'scroll',
+      () => setPage(-Math.round((firstPage.getBoundingClientRect().top - container.getBoundingClientRect().top) / 550)),
+      false
+    );
+  }, []);
+
   return (
     <div className='w-screen h-screen text-white bg-noise relative p-12 flex items-center justify-center'>
       <Logo />
       <Links />
-      <Intro />
+      <div
+        id='container'
+        className='absolute top-1/2 -translate-y-1/2 left-44 w-5/6 h-[550px] overflow-y-scroll scrollbar-hide snap-y snap-mandatory'
+      >
+        <Intro id='intro' />
+        <Intro id='about' />
+        <Intro id='skills' />
+        <Intro id='work' />
+      </div>
+      <Pagination page={page}/>
     </div>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Intro() {
+export default function Intro({ id }) {
 
   return (
-    <div className='flex flex-col w-5/6 h-5/6 justify-center pl-12 space-y-5 text-sm'>
+    <div id={id} className='flex flex-col w-full h-full justify-center pl-12 space-y-5 text-sm snap-center'>
       <p className='text-lg'>Introduction</p>
       <h1 className='text-5xl font-medium'>Machine Learning Engineer</h1>
       <p className='text-white/70 w-1/2 leading-loose'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat mauris eu tellus porta, eget suscipit odio dictum.</p>
